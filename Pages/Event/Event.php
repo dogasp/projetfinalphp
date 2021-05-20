ecb76621033b86596d0ba452fae5c799819e94af
auphangwen <auphangwen@eisti.eu>
2021-05-20 02:25:14 +0200

    yo

--- Pages/Event/Event.php
+++ Pages/Event/Event.php
@@ -12,7 +12,7 @@
     <link rel="stylesheet" href="style.css">
     <script src="event.js"></script>
     <script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-</head>
+</head> 
 <body>
 <div id = "top_page">
     <h1>Évenements organisés par votre BDS</h1>
@@ -21,6 +21,16 @@
     Vous êtes sur la bonne page! <br>
     Pour vous y inscrire c'est juste en dessous: </span></div>
     </div> 
+</div>
+    <div class="menu">
+    <ul id="nav">
+        <li><a href="../../index.php">Accueil</a></li>
+        <li><a href="../../Pages/Event/Event.php">Events</a></li>
+        <li><a href="../../Pages/Magasin/Magasin.php">Magasin</a></li>
+        <li><a href="../../Pages/Admin/Admin.php">Utilisateurs</a></li>
+    </ul>
+    </li>
+    </div>
     <div class = "container">
 
     <?php
